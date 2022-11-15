3eb36465e996757d8abc13f9280a138fec3da0ae
neta shiff <netashiff1@gmail.com>
2022-11-15 13:01:39 -0500

    Update dropzone

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,7 +8,11 @@ from flaskr.db import get_db
 
 from flaskr.foliummaps import create_map_html
 
+from auth import *
+
+jumpMapDB= client['JumpMap']
 bp = Blueprint('blog', __name__)
+
 
 @bp.route('/')
 def index():
@@ -23,6 +27,7 @@ def index():
     folium_map = create_map_html(start_coords)
 
     return render_template('blog/index.html', posts=posts, folium_map=folium_map)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -49,6 +54,7 @@ def create():
 
     return render_template('blog/create.html')
 
+
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -64,6 +70,7 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
@@ -92,6 +99,7 @@ def update(id):
 
     return render_template('blog/update.html', post=post)
 
+
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
@@ -101,3 +109,40 @@ def delete(id):
     db.commit()
     return redirect(url_for('blog.index'))
 
+
+@bp.route('/newdropzone', methods=('GET', 'POST'))
+@login_required
+def add_dropzone():
+    if request.method == 'POST':
+        Zone_name = request.form['Zone_name']
+        State= request.form['State']
+        City = request.form['City']
+        Latitude = request.form['Latitude']
+        Longitude = request.form['Longitude']
+        img = request.form['img']
+        db = get_db()
+        error = None
+
+        if not Zone_name:
+            Zone_name = 'Zone_name is required.'
+
+        if error is None:
+            try:
+                Dropzone_collection = jumpMapDB['Dropzones']
+                DZ_info = {"Zone_name": Zone_name,
+                               "State": State,
+                               "City": City,
+                               "Latitude": Latitude,
+                               "Longitude": Longitude,
+                               "img": img,
+                               "Date Created": datetime.datetime.utcnow()}
+                document =Dropzone_collection.insert_one(DZ_info).inserted_id
+                print(jumpMapDB.list_collection_names())
+            except db.IntegrityError:
+                error = f"Dropzone {Zone_name} is already registered."
+            else:
+                return redirect(url_for("blog.create"))
+
+        flash(error)
+
+    return render_template('blog/New_dropzone.html')
